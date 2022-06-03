09b41f78159d4c0c1f730e5896dcd52aa800d6d7
ArthurVieillevoye <56159647+ArthurVieillevoye@users.noreply.github.com>
2022-06-03 11:06:50 +0200

    Interval Newton Method updates

--- EigenComputation/IntervalNewtonMethod.py
+++ EigenComputation/IntervalNewtonMethod.py
@@ -10,7 +10,7 @@ def f(A, v, lamb):
     # print(len(c))
     # print(transpose(c))
     # print(type(1 - (transpose(v) * v)))
-    f = join(c, 1 - (transpose(v) * v))
+    f = join(c, 1 - (transpose(v) * v)/FloatDPApproximation(2, dp)) # TODO: need to divide by 2 the vT*v
     return f
 
 
@@ -19,37 +19,60 @@ def df(A, v, lamb):
     I = FloatDPBoundsMatrix.identity(n, dp)
     # print(type(A - (lamb * I)))
     # print(type(FloatDPBounds((-1), dp)*v))
-    topRow = cojoin(A - (lamb * I), FloatDPBounds((-1), dp)*v)
+    topRow = cojoin(A - (lamb * I), -v)
 
     print(topRow)
 
-    bottomRow = cojoin(FloatDPBounds((-1), dp)*transpose(v), FloatDPBounds(0, dp))
+    bottomRow = cojoin(transpose(-v), FloatDPBounds(0, dp))
     # bottomRow = transpose(bottomRow)
 
     print(type(topRow), '##### ', type(bottomRow))
     F = join(topRow, bottomRow)
     return F
 
+def getIFirstElementsInVector(v, i):
+    smallerV = []
+    for j in range(i):
+        smallerV.append(v[j])
+    smallerV= FloatDPBoundsVector(smallerV, dp)
+    return smallerV
 
-def intervalNewtonMethod(A, vect, lamb):
-    assert (type(lamb) == FloatDPBounds or type(lamb) == FloatMPBounds)
-    e = 1
-    # e = FloatDPBoundsVector([FloatDPBounds(3, 1, dp), FloatDPBounds(3, 1, dp)])
 
-    v1 = vect + e
-    v2 = (vect + e) / FloatDPBounds(2, dp)
+def maxLenghtBounds(v):
+    maxRange = FloatDPApproximation(v[0].upper()) - FloatDPApproximation(v[0].lower())
+    for i in range (1, len(v)):
+        if FloatDPApproximation(v[i].upper()) - FloatDPApproximation(v[i].lower()) > maxRange:
+            maxRange = FloatDPApproximation(v[i].upper()) - FloatDPApproximation(v[i].lower())
 
-    v = join(v1, v2)
+    return maxRange
 
-    v = cast_exact(v) - solve(df(A, v, lamb), f(A, v, lamb))
+
+# def intervalNewtonMethod(A, vect, lamb):
+#     assert (type(lamb) == FloatDPBounds or type(lamb) == FloatMPBounds)
+#     e = 1
+#     # e = FloatDPBoundsVector([FloatDPBounds(3, 1, dp), FloatDPBounds(3, 1, dp)])
+#
+#     v1 = vect + e
+#     v2 = (vect + e) / FloatDPBounds(2, dp)
+#
+#     v = join(v1, v2)
+#
+#     v = cast_exact(v) - solve(df(A, v, lamb), f(A, v, lamb))
 
 
 if __name__ == '__main__':
-    A = FloatDPBoundsMatrix([[2,-1,1],[-1,3,2],[1,2,3]], dp)
-    eps = 0.5
-    v = FloatDPBoundsVector([{x_(1-eps):x_(1+eps)},{x_(-1):x_(-1+eps)},{x_(-1):x_(0+eps)}], dp)
-    lamb = FloatDPBounds({1:2}, dp)
+    #A = FloatDPBoundsMatrix([[2,-1,1],[-1,3,2],[1,2,3]], dp)
+    A = FloatDPBoundsMatrix([[3,-1,1],[-1,4,2],[1,2,5]], dp)
+    eps = 0.001
+    eps=0
+    # v = FloatDPBoundsVector([{x_(1 - eps): x_(1 + eps)}, {x_(-1): x_(-1 + eps)}, {x_(-1): x_(-1 + eps)}], dp)
+    v = FloatDPBoundsVector([{63:64},{59:60},{-50:-49}],dp)/FloatDPBounds(100,dp)
 
+    # lamb = FloatDPBounds(Decimal(1.2),Decimal(1.3),dp)
+    lamb = FloatDPBounds(10, 11, dp) / 10
+
+    print("v:",v)
+    print("inv(A)",inverse(A))
     assert (type(lamb) == FloatDPBounds or type(lamb) == FloatMPBounds)
     # e = 1
     # e = FloatDPBoundsVector([1, 1], dp)
@@ -57,29 +80,51 @@ if __name__ == '__main__':
     # v1 = v - e
     # v2 = (v + e)
 
+
+    # I=FloatDPBoundsMatrix.identity(3,dp)
+    # z=FloatDPBounds(0,dp)
+    # J=join(cojoin(A-lamb*I,v),cojoin(transpose(v),z))
+    # print("J:", J)
+    # print("inv(J)",inverse(J))
     # v = join(v1, v2)
     # print(len(v))
     # print(v)
+    precision = 0.0001
+    while maxLenghtBounds(v) > precision:
+        # TODO: find the boolean value.
 
-    df = df(A, v, lamb)
-    f = f(A, v, lamb)
-    print(df.column_size(), ' ++++++', df.row_size())
-    print(len(f))
+        dfx = df(A, v, lamb)
+        fx = f(A, FloatDPBoundsVector(cast_exact(v)), FloatDPBounds(cast_exact(lamb)))
+        # print(df.column_size(), ' ++++++', df.row_size())
+        print(len(fx))
 
-    tmp = solve(df, f)
-    print(type(tmp))
-    print(tmp)
+        print("dfx:", dfx, type(dfx))
+        print("fx:", fx, type(fx))
 
-    print((cast_exact(v)))
-    print(lamb)
+        print("inv(dfx):",inverse(dfx))
+        deltax = solve(dfx, fx)
+        print(type(deltax))
+        print("deltax:",deltax,type(deltax))
 
-    v = cast_exact(v) - tmp
-    #
-    # print(refines(v1, v2))
-    #
-    # if refines(v1, v2):
-    #     print('we got a problem')
+        print((cast_exact(v)))
+        print(lamb)
 
-    # print(cast_exact(e))
-# else:
-#	print('All right')
+        lambBound = deltax[len(deltax)-1]
+        vBound = getIFirstElementsInVector(deltax, len(deltax)-1)
+        print("vBounds", vBound)
+        newV = cast_exact(v) - vBound
+
+        print("oldV: ", v)
+        print("newV: ", newV)
+        if inconsistent(newV, v):
+            print("There is no solutions")
+        elif refines(newV, v):
+            print("There is only one solution")
+            newV = refinement(newV, v)
+        else:
+            newV = refinement(newV, v)
+
+        break
+
+    print("newV:", newV)
+
--- UI/NewGUI.py
+++ UI/NewGUI.py
@@ -20,8 +20,8 @@ textArea = TextArea.MainTextArea()
 textArea.addOutputArea(root)
 
 # Add the top menu with all the functionalities.
-menuBar = MenuTopBar.MenuBar(textArea)
-menuBar.addMenuBar(root)
+# menuBar = MenuTopBar.MenuBar(textArea)
+# menuBar.addMenuBar(root)
 
 # Add the left menu with the different buttons
 leftMenu = MenuLeftBar.MenuWithButton(root, textArea)
