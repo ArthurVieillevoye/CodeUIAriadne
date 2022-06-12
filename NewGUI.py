cd89d7a9bff63a2a2eaf5704754d328f42157628
ArthurVieillevoye <56159647+ArthurVieillevoye@users.noreply.github.com>
2022-06-12 13:48:21 +0200

    Cleaned NewGUI

--- NewGUI.py
+++ NewGUI.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from UI.Menu import MenuTopBar
 from UI.Text import TextArea
 from UI.Menu import MenuLeftBar
 
@@ -7,31 +6,21 @@ from UI.Menu import MenuLeftBar
 This is the Main file of the programm. This launch the User Interface.
 """
 
-# Create the window on which everything is going to be visible.
-root = Tk()
-root.title('Ariadne')
-# root.geometry("200x200")
-# root.geometry("{0}x{1}+0+0".format((root.winfo_screenwidth()),
-#                                    (root.winfo_screenheight())))
+if __name__ == "__main__":
+    # Create the window on which everything is going to be visible.
+    root = Tk()
+    root.title('Ariadne')
 
-# Add the text area as well as the line count
-textArea = TextArea.MainTextArea()
-# textArea.addTextArea(root)
-textArea.addOutputArea(root)
+    # Add the text area as well as the line count
+    textArea = TextArea.MainTextArea()
+    # textArea.addTextArea(root)
+    textArea.addOutputArea(root)
 
-# Add the top menu with all the functionalities.
-# menuBar = MenuTopBar.MenuBar(textArea)
-# menuBar.addMenuBar(root)
+    # Add the left menu with the different buttons
+    leftMenu = MenuLeftBar.MenuWithButton(root, textArea)
+    leftMenu.addButtonMenu()
 
-# Add the left menu with the different buttons
-leftMenu = MenuLeftBar.MenuWithButton(root, textArea)
-leftMenu.addButtonMenu()
+    status = Label(root, text="", anchor=E)
+    status.grid(row=2, column=1, columnspan=2, sticky="EW")
 
-# outputTextArea = TextArea.MainTextArea()
-# outputTextArea.addOutputArea(root)
-
-
-status = Label(root, text="Need to put the status here", anchor=E)
-status.grid(row=2, column=1, columnspan=2, sticky="EW")
-
-root.mainloop()
+    root.mainloop()
